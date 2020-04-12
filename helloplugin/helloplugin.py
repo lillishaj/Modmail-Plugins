88afec6b3c163e8e4ab04db214758562d67c4d74
lillishaj <32970249+lillishaj@users.noreply.github.com>
2020-04-12 21:04:42 +0200

    testar

--- helloplugin/helloplugin.py
+++ helloplugin/helloplugin.py
@@ -9,24 +9,24 @@ class HelloPlugin(commands.Cog):
         if message.author.bot:
             return
 
-        if "hello" in message.content.lower():
-            await message.channel.send("Hey")
-        elif "yo" in message.content.lower():
-            await message.channel.send("yo")
+        if "hej" in message.content.lower():
+            await message.channel.send("Hej")
+        elif "tja" in message.content.lower():
+            await message.channel.send("Tjena!")
         elif "gm" in message.content.lower():
-            await message.channel.send("Good Morning")
+            await message.channel.send("God morgon")
         elif "gn" in message.content.lower():
-            await message.channel.send("Good Night")
-        elif "good morning" in message.content.lower():
-            await message.channel.send("Good Morning !")
-        elif "good night" in message.content.lower():
-            await message.channel.send("Good Night !")
-        elif "hey" in message.content.lower():
-            await message.channel.send("Hello !")
-        elif "sup" in message.content.lower():
-            await message.channel.send("Sup")
-        elif "hi" in message.content.lower():
-            await message.channel.send("Hello!")
+            await message.channel.send("God natt")
+        elif "god morgon" in message.content.lower():
+            await message.channel.send("God morgon!")
+        elif "god natt" in message.content.lower():
+            await message.channel.send("God natt!")
+        elif "hejsan" in message.content.lower():
+            await message.channel.send("Hejsan!")
+        elif "tjo" in message.content.lower():
+            await message.channel.send("Tja")
+        elif "wtf" in message.content.lower():
+            await message.channel.send("Inte svära!")
 
 def setup(bot):
     bot.add_cog(HelloPlugin(bot))
